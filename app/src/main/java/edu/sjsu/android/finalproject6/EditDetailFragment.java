675ed6ac9b3d4c0c52198b193e95e99839e109fe
Timothy Nguyen <113748439+TimothyCNguyen@users.noreply.github.com>
2024-12-06 15:43:11 -0800

    Update EditDetailFragment.java
    Added import Toast.

--- app/src/main/java/edu/sjsu/android/finalproject6/EditDetailFragment.java
+++ app/src/main/java/edu/sjsu/android/finalproject6/EditDetailFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import edu.sjsu.android.finalproject6.databinding.FragmentEditDetailBinding;
 
